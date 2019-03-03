be54ac1fc8e61bd76e082c2e0ae6f99cb97338c4
Kristin Jackvony <kristin.jackvony@nextsteplivinginc.com>
2019-03-02 23:04:10 -0500

    Initial commit

---
+++ api/models/contactListModel.js
@@ -0,0 +1,37 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ContactSchema = new Schema({
+  firstName: {
+    type: String,
+    required: 'First Name is required'
+  },
+  lastName: {
+    type: String,
+    required: 'Last Name is required'
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  streetAddress1: {
+    type: String,
+  },
+  streetAddress2: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  state: {
+    type: String,
+  },
+  postalCode: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('Contacts', ContactSchema);
---
+++ api/routes/contactListRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  var contactList = require('../controllers/contactListController');
+
+  // todoList Routes
+  app.route('/contacts')
+    .get(contactList.list_all_contacts)
+    .post(contactList.create_a_contact);
+
+
+  app.route('/contacts/:contactId')
+    .get(contactList.read_a_contact)
+    .put(contactList.update_a_contact)
+    .delete(contactList.delete_a_contact);
+};
