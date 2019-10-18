e42c04aca63ac76efc75bc4a61daf867a11f2902
Kristin Jackvony <kristin.jackvony@nextsteplivinginc.com>
2019-10-17 21:34:53 -0400

    Changing address to use nested JSON

--- api/models/contactListModel.js
+++ api/models/contactListModel.js
@@ -10,7 +10,7 @@ var ContactSchema = new Schema({
     validate: {
       validator: function(v) {
         return /^[a-zA-Z'-]+( [a-zA-Z'-]+)*$/.test(v);
-      },
+      }
     }
   },
   lastName: {
@@ -20,7 +20,7 @@ var ContactSchema = new Schema({
     validate: {
       validator: function(v) {
         return /^[a-zA-Z'-]+( [a-zA-Z'-]+)*$/.test(v);
-      },
+      }
     }
   },
   email: {
@@ -30,7 +30,7 @@ var ContactSchema = new Schema({
     validate: {
       validator: function(v) {
         return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,})?$/.test(v);
-      },
+      }
     }
   },
   phone: {
@@ -38,53 +38,58 @@ var ContactSchema = new Schema({
     validate: {
       validator: function(v) {
         return /^\d{10}$/.test(v);
-      },
+      }
     }
   },
-  streetAddress1: {
-    type: String,
-    maxlength: 30,
-    validate: {
-      validator: function(v) {
-        return /^[a-zA-Z0-9'.-]+( [a-zA-Z0-9'.-]+)*$/.test(v);
+  address: {
+    type: "object",
+    properties: {
+      streetAddress1: {
+        type: String,
+        maxlength: 30,
+        validate: {
+          validator: function(v) {
+            return /^[a-zA-Z0-9'.-]+( [a-zA-Z0-9'.-]+)*$/.test(v);
+          }
+        }
       },
-    }
-  },
-  streetAddress2: {
-    type: String,
-    maxlength: 20,
-    validate: {
-      validator: function(v) {
-        return /^[a-zA-Z0-9'.-]+( [a-zA-Z0-9'.-]+)*$/.test(v);
+      streetAddress2: {
+        type: String,
+        maxlength: 20,
+        validate: {
+          validator: function(v) {
+            return /^[a-zA-Z0-9'.-]+( [a-zA-Z0-9'.-]+)*$/.test(v);
+          }
+        }
       },
-    }
-  },
-  city: {
-    type: String,
-    maxlength: 30,
-    validate: {
-      validator: function(v) {
-        return /^[a-zA-Z0-9'-.]+( [a-zA-Z0-9'-.]+)*$/.test(v);
+      city: {
+        type: String,
+        maxlength: 30,
+        validate: {
+          validator: function(v) {
+            return /^[a-zA-Z0-9'-.]+( [a-zA-Z0-9'-.]+)*$/.test(v);
+          }
+        }
       },
-    }
-  },
-  state: {
-    type: String,
-    maxlength: 2,
-    validate: {
-      validator: function(v) {
-        return /\b[a-zA-Z]{2}\b/.test(v);
+      state: {
+        type: String,
+        maxlength: 2,
+        validate: {
+          validator: function(v) {
+            return /\b[a-zA-Z]{2}\b/.test(v);
+          }
+        }
       },
+      postalCode: {
+        type: String,
+        validate: {
+          validator: function(v) {
+            return /^[0-9]{5}(?:-[0-9]{4})?$/.test(v);
+          }
+        }
+      }
     }
-  },
-  postalCode: {
-    type: String,
-    validate: {
-      validator: function(v) {
-        return /^[0-9]{5}(?:-[0-9]{4})?$/.test(v);
-      },
-    }
-  },
+  }  
 });
 
 module.exports = mongoose.model('Contacts', ContactSchema);
